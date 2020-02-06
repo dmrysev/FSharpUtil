1186dc652ca8716e6f2de506efb4f87f8ceb5af6
dmrysev <dmrysev@gmail.com>
2020-02-06 16:19:14 +0200

    In String module renamed stripChars -> strip

--- Src/String.fs
+++ Src/String.fs
@@ -1,6 +1,6 @@
 module Util.String
 
-let stripChars chars str =
+let strip chars str =
     Seq.fold(fun (str: string) chr ->
         str.Replace(chr |> System.Char.ToUpper |> string, "").Replace(chr |> System.Char.ToLower |> string, ""))
         str chars
