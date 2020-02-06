3b14724ad850fa422d67e8469ec8592ce8ed2584
dmrysev <dmrysev@gmail.com>
2020-02-06 13:56:46 +0200

    Added test case

--- Test/Math.Test.fs
+++ Test/Math.Test.fs
@@ -3,6 +3,7 @@ module Util.Math.Test
 open NUnit.Framework
 open FsUnit
 
-[<Test>]
-let ``Given float number, calling decimalPart, must return decimal part of that number`` () =
-    decimalPart 123.135 |> should (equalWithin 0.0001) 0.135
+[<TestCase(1.0, 0.0)>]
+[<TestCase(123.135, 0.135)>]
+let ``Given float number, calling decimalPart, must return decimal part of that number`` (input, expected) =
+    decimalPart input |> should (equalWithin 0.0001) expected
