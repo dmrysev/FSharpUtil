616d0a335e5e919e6ea6cf51bcb37eb3c942f27d
dmrysev <dmrysev@gmail.com>
2020-02-06 13:59:54 +0200

    Added test for Math

--- Test/Math.Test.fs
+++ Test/Math.Test.fs
@@ -7,3 +7,8 @@ open FsUnit
 [<TestCase(123.135, 0.135)>]
 let ``Given float number, calling decimalPart, must return decimal part of that number`` (input, expected) =
     decimalPart input |> should (equalWithin 0.0001) expected
+
+[<TestCase(1.0, false)>]
+[<TestCase(1.5, true)>]
+let ``Given float number, calling hasDecimals, must return true if it has decimals or false if it doesn't have`` (input, expected) =
+    hasDecimals input |> should equal expected
