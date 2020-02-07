396d3851dadce1ac95b1eb2df2cb479774b5a0f0
dmrysev <dmrysev@gmail.com>
2020-02-07 13:17:29 +0200

    Added test cases for String.Test

--- Test/String.Test.fs
+++ Test/String.Test.fs
@@ -3,6 +3,11 @@ module Util.String.Test
 open NUnit.Framework
 open FsUnit
 
-[<TestCase("fafaf", [|'a'|], "fff")>]
+[<TestCase("aaa", "a", "")>]
+[<TestCase("fafaaf", "a", "fff")>]
+[<TestCase("fafaAf", "a", "fff")>]
+[<TestCase("fafAAf", "a", "fff")>]
+[<TestCase("fafAAf", "A", "fff")>]
+[<TestCase("fafbfaafbbfabf", "ab", "ffffff")>]
 let ``Given string and character, calling strip, must remove all characters from that string`` (inputString, characters, expected) =
     Util.String.strip characters inputString |> should equal expected
