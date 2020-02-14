0ffef0e748f82536465de7b18f97de2fe1ed6cbb
dmrysev <dmrysev@gmail.com>
2020-02-14 16:15:51 +0200

    Added html test

--- Test/Html.Test.fs
+++ Test/Html.Test.fs
@@ -6,8 +6,8 @@ open FSharp.Data
 
 [<Test>]
 let ``Given html document, calling findLinks, must return list of links and their names`` () =
-    let content = "<!DOCTYPE html>\
-        <html lang=en\">\
+    let content = "
+        <html>\
         <body>\
             <a href=\"test_link_1\">Test text 1</a>\
             <a href=\"test_link_2\">Test text 2</a>\ 
@@ -16,3 +16,16 @@ let ``Given html document, calling findLinks, must return list of links and thei
 
     let html = HtmlDocument.Parse content
     findLinks html |> should equivalent [("Test text 1", "test_link_1"); ("Test text 2", "test_link_2")]
+
+[<Test>]
+let ``Given html document, calling findImages, must return list of image links and their names`` () =
+    let content = "
+        <html>\
+        <body>\
+            <img src=\"test_link_1\" alt=\"Test text 1\"/>\
+            <img src=\"test_link_2\" alt=\"Test text 2\"/>\
+        </body>\
+        </html>"
+
+    let html = HtmlDocument.Parse content
+    findImages html |> should equivalent [("Test text 1", "test_link_1"); ("Test text 2", "test_link_2")]
