61249ce18270cbd938cc453dcdd90053b1b5cff0
dmrysev <dmrysev@gmail.com>
2020-02-17 16:53:58 +0200

    In Http.downloadRawContent added possibility to specify additional cookies

--- Src/Http.fs
+++ Src/Http.fs
@@ -3,12 +3,16 @@ module Util.Http
 open FSharp.Data
 open System.IO
 
-let downloadRawContent pageUrl =
-    let cookies = 
-        Http.Request(pageUrl).Cookies
+let downloadRawContent (pageUrl: string) (additionalCookies: seq<string*string> option) =
+    let userCookies = 
+        match additionalCookies with
+        | Some c -> c
+        | None -> Seq.empty
+    let allCookies = 
+        Http.Request(pageUrl).Cookies 
         |> Map.toSeq
-    Http.RequestString(pageUrl, httpMethod = "GET", cookies = cookies)
+        |> Seq.append userCookies
+    Http.RequestString(pageUrl, httpMethod = "GET", cookies = allCookies)
 
-let getHtmlDocument url = 
-    let content = downloadRawContent url
+let getHtmlDocument (url: string) = 
     HtmlDocument.Load url
