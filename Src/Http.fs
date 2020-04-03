1b82536faebf614f59dad35966e7f7d1909b7a41
dmrysev <dmrysev@gmail.com>
2020-04-03 19:37:31 +0300

    In Http.fs added downloadBinaryFileAsync

--- Src/Http.fs
+++ Src/Http.fs
@@ -7,3 +7,9 @@ let downloadBinaryFile url outputFilePath =
     match Http.Request(url).Body with
     | Text _ -> failwith "Could not download binary"
     | Binary bytes -> File.WriteAllBytes(outputFilePath, bytes)
+
+let downloadBinaryFileAsync url output = async {
+    let! response = Http.AsyncRequest(url)
+    match response.Body with
+    | Text t -> failwith "Could not download binary"
+    | Binary bytes -> File.WriteAllBytes(output, bytes) }
