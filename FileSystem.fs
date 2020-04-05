08b8575628f9986070232503bb6211e4fcb80a65
dmrysev <dmrysev@gmail.com>
2020-04-05 23:27:08 +0300

    In FileSystem added findAvailablePathWithAppendix

--- FileSystem.fs
+++ FileSystem.fs
@@ -9,4 +9,12 @@ let fixFileName (fileName: string) =
     Util.String.strip invalidChars fileName
 
 let isDirectoryEmpty (folderPath: string) =
-    Directory.GetFileSystemEntries(folderPath).Length = 0    
+    Directory.GetFileSystemEntries(folderPath).Length = 0
+
+let findAvailablePathWithAppendix (filePath: string) = 
+    let rec findAppendix number =
+        let newFilePath = sprintf "%s_%i" filePath number
+        if File.Exists(newFilePath) || Directory.Exists(newFilePath)
+        then findAppendix (number + 1)
+        else newFilePath
+    findAppendix 1
