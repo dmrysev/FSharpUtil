d7bd861a1581da553549ad1b96efabe2100391a7
dmrysev <dmrysev@gmail.com>
2020-08-18 14:46:51 +0300

    In FileSystem.fs fixFileName now fixes both for windows and linux regardless where it is run

--- FileSystem.fs
+++ FileSystem.fs
@@ -5,7 +5,7 @@ open System.IO
 let (/) path1 path2 = Path.Combine(path1, path2)
 
 let fixFileName (fileName: string) =
-    let invalidChars = Path.GetInvalidFileNameChars()
+    let invalidChars = ['/'; '<'; '>'; ':'; '"'; '/'; '\\'; '|'; '?'; '*']
     Util.String.strip invalidChars fileName
 
 let isDirectoryEmpty (folderPath: string) =
