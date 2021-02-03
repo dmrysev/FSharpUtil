ca7d97378b926a558e0579d6b56a82fc3fb7c2a3
dmrysev <dmrysev@gmail.com>
2021-02-03 21:01:11 +0200

    In Http.downloadBinaryFile auto create directory

--- Http.fs
+++ Http.fs
@@ -3,7 +3,9 @@ module Util.Http
 open FSharp.Data
 open System.IO
 
-let downloadBinaryFile url outputFilePath =
+let downloadBinaryFile (url: string) (outputFilePath: string) =
+    let dirPath = Path.GetDirectoryName(outputFilePath)
+    if not <| (Directory.Exists dirPath) then Directory.CreateDirectory dirPath |> ignore
     match Http.Request(url).Body with
     | Text text -> failwith (sprintf "Could not download binary.\nServer returned\n%s" text)
     | Binary bytes -> File.WriteAllBytes(outputFilePath, bytes)
