9a6365e7e9f02bf8c06e85f6afc6b61f609ef6be
dmrysev <dmrysev@gmail.com>
2021-02-19 19:28:52 +0200

    Refactored

--- IO.fs
+++ IO.fs
@@ -3,7 +3,8 @@ module Util.IO
 open System.IO
 
 module File =
-    let append (filePath: string) (text: string) =
+    let appendLine (filePath: string) (text: string) =
         use fileStream = File.Open(filePath, FileMode.Append)
         use streamWriter = new StreamWriter(fileStream)
         streamWriter.WriteLine(text)
+        
