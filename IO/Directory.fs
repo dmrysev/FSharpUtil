dc22da2755f1b246b38fac37bcb8c4669acab9ae
dmrysev <dmrysev@gmail.com>
2021-02-19 20:26:27 +0200

    Removed FileSysyem.fs. Added IO/Directory

---
+++ IO/Directory.fs
@@ -0,0 +1,7 @@
+module Util.IO.Directory
+
+let empty (folderPath: string) =
+    System.IO.Directory.GetFileSystemEntries(folderPath).Length = 0
+
+let countFiles (folderPath: string) =
+    System.IO.Directory.EnumerateFiles folderPath |> Seq.length
--- IO/Path.fs
+++ IO/Path.fs
@@ -1,3 +1,18 @@
 module Util.IO.Path
 
 let (/) path1 path2 = System.IO.Path.Combine(path1, path2)
+
+let fixFileName (fileName: string) =
+    let invalidChars = ['/'; '<'; '>'; ':'; '"'; '/'; '\\'; '|'; '?'; '*']
+    Util.String.strip invalidChars fileName
+
+let exists (path: string) = 
+    System.IO.Directory.Exists path || System.IO.File.Exists path
+
+let findAvailablePathWithAppendix (filePath: string) = 
+    let rec findAppendix number =
+        let newFilePath = sprintf "%s_%i" filePath number
+        if exists newFilePath
+        then findAppendix (number + 1)
+        else newFilePath
+    findAppendix 1
