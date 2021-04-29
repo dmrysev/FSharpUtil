605be65690f0f4bd5acd45872f005b4acce37315
dmrysev <dmrysev@protonmail.com>
2021-04-29 21:35:17 +0300

    Fixed version control

--- VersionControl.fs
+++ VersionControl.fs
@@ -1,15 +1,23 @@
 module Util.VersionControl
 
-let init(dirPath: string) =
-    let proc = Util.Process.run "git init"
-    proc.WaitForExit()
+let run (workingDirectory: string) (command: string) =
+    let p = new System.Diagnostics.Process()
+    p.StartInfo.RedirectStandardOutput <- false
+    p.StartInfo.UseShellExecute <- false
+    p.StartInfo.WorkingDirectory <- workingDirectory
+    p.StartInfo.FileName <- "/bin/bash"
+    let arguments = sprintf "-c \"%s\"" command
+    p.StartInfo.Arguments <- arguments
+    p.Start() |> ignore    
+    p
 
 let add(filePath: string) = 
+    let workingDirectory = System.IO.Path.GetDirectoryName filePath
     let command = sprintf "git add %s" filePath
-    let proc = Util.Process.run command
+    let proc = run workingDirectory command
     proc.WaitForExit()
 
-let commit(message: string) = 
-    let command = sprintf "git commit -m '%s'" message
-    let proc = Util.Process.run command
+let commit (workingDirectory: string) (message: string) = 
+    let command = sprintf "git commit -a -m '%s'" message
+    let proc = run workingDirectory command
     proc.WaitForExit()
