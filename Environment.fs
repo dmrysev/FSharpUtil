9135f9aa69bc75e61d4cdff1646841a6942d4452
dmrysev <dmrysev@protonmail.com>
2021-07-24 15:48:17 +0300

    Added Environment.OS module

--- Environment.fs
+++ Environment.fs
@@ -1,7 +1,13 @@
 module Util.Environment
 
 open Util.IO.Path
+open System.Runtime.InteropServices
 
 module SpecialFolder =
-    let homeDirPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)
-    let applicationDataDirPath = homeDirPath/".local/share/LinuxUtil"
+    let home = System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)
+    let applicationData = home/".local/share"
+    let temporary = "/tmp"
+
+module OS =
+    let isLinux() =
+        System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
