3dad51fcf4a81a3ffd16f6fe47fb1e9eb590766b
dmrysev <dmrysev@protonmail.com>
2021-07-31 14:02:03 +0300

    In Process.execute fixed running quoted commands

--- Process.fs
+++ Process.fs
@@ -11,14 +11,17 @@ let run (command: string) =
     p
 
 let execute (command: string) =
+    let guid = System.Guid.NewGuid().ToString()
+    let temporaryScriptFile = "/tmp/" + guid + ".sh"
+    System.IO.File.WriteAllText(temporaryScriptFile, command)
     let p = new System.Diagnostics.Process()
     p.StartInfo.RedirectStandardOutput <- true
     p.StartInfo.UseShellExecute <- false
     p.StartInfo.FileName <- "/bin/bash"
-    let arguments = sprintf "-c \"%s\"" command
-    p.StartInfo.Arguments <- arguments
+    p.StartInfo.Arguments <- temporaryScriptFile
     p.Start() |> ignore    
     let reader = p.StandardOutput
     let output = reader.ReadToEnd()
     p.WaitForExit()
+    System.IO.File.Delete temporaryScriptFile
     output
