c59f4255378555d2869c26a23daa4fa4e5109513
dmrysev <dmrysev@protonmail.com>
2021-07-31 12:22:08 +0300

    Added IO.Process.execute

--- Process.fs
+++ Process.fs
@@ -9,3 +9,16 @@ let run (command: string) =
     p.StartInfo.Arguments <- arguments
     p.Start() |> ignore    
     p
+
+let execute (command: string) =
+    let p = new System.Diagnostics.Process()
+    p.StartInfo.RedirectStandardOutput <- true
+    p.StartInfo.UseShellExecute <- false
+    p.StartInfo.FileName <- "/bin/bash"
+    let arguments = sprintf "-c \"%s\"" command
+    p.StartInfo.Arguments <- arguments
+    p.Start() |> ignore    
+    let reader = p.StandardOutput
+    let output = reader.ReadToEnd()
+    p.WaitForExit()
+    output
