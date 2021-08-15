317ba4f34bd30056ff480dc2611dddccc2eea0d0
dmrysev <dmrysev@protonmail.com>
2021-08-15 14:33:37 +0300

    Added MessageQueue tests

--- Test/MessageQueue.Test.fs
+++ Test/MessageQueue.Test.fs
@@ -5,10 +5,12 @@ open NUnit.Framework
 open FsUnit
 
 let queue1 = "queue_1"
+let queue2 = "queue_1"
 
 [<SetUp>]
 let setUp() =
     resetQueue queue1
+    resetQueue queue2
 
 [<Test>]
 let ``MessageQueue must satisfy all FIFO property``() =
@@ -18,3 +20,25 @@ let ``MessageQueue must satisfy all FIFO property``() =
     dequeue queue1 |> should equal "message 1"
     dequeue queue1 |> should equal "message 2"
     dequeue queue1 |> should equal "message 3"
+
+[<Test>]
+let ``MessageQueue must support multiple queues``() =
+    enqueue queue1 "message 1"
+    enqueue queue2 "message 2"
+    dequeue queue1 |> should equal "message 1"
+    dequeue queue2 |> should equal "message 2"
+
+[<Test>]
+let ``MessageQueue must support async operations for same queue``() =
+    [enqueueAsync queue1 "message 1"; enqueueAsync queue1 "message 2"]
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+
+    let result =
+        dequeueAsync queue1
+        |> Seq.replicate 2
+        |> Async.Parallel
+        |> Async.RunSynchronously
+
+    result |> should equivalent ["message 1"; "message 2"]
