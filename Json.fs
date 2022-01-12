35ddc820d40c025858bfc761f3fff5da4d0d9682
dmrysev <dmrysev@protonmail.com>
2022-01-12 20:55:00 +0200

    Added json functions

--- Json.fs
+++ Json.fs
@@ -1,7 +1,10 @@
 module Util.Json
 
+open Util.IO.Path
 open Newtonsoft.Json
 
 let toJson message = JsonConvert.SerializeObject message
 let fromJson<'a> json = JsonConvert.DeserializeObject<'a> json
 let parse text = Linq.JObject.Parse text
+let read (jsonFilePath: FilePath) = Util.IO.File.readAllText jsonFilePath |> parse
+let deserializeFile<'a> (jsonFilePath: FilePath) = Util.IO.File.readAllText jsonFilePath |> fromJson<'a>
