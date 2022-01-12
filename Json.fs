79bc3952e00b4796073b9f883004509557256e2c
dmrysev <dmrysev@protonmail.com>
2022-01-12 17:54:50 +0200

    Added Json.parse

--- Json.fs
+++ Json.fs
@@ -4,3 +4,4 @@ open Newtonsoft.Json
 
 let toJson message = JsonConvert.SerializeObject message
 let fromJson<'a> json = JsonConvert.DeserializeObject<'a> json
+let parse text = Linq.JObject.Parse text
