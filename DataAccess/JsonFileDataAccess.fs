81e88e4d2a00ed3a366b9e77cd97d5c3e4212ea7
dmrysev <dmrysev@protonmail.com>
2022-01-28 15:49:12 +0200

    Added Util.DataAccess.JsonFileDataAccess

---
+++ DataAccess/JsonFileDataAccess.fs
@@ -0,0 +1,46 @@
+namespace Util.DataAccess
+
+open Util.IO.Path
+
+type JsonFileDataAccess (dataEntriesDirPath: DirectoryPath) =
+    let jsonFieldValueComparer (fieldName: string) value (jsonFilePath: FilePath) =
+        let json = Util.Json.read jsonFilePath
+        json.Value(fieldName) = value 
+
+    let getEntryFilePath id =
+        let fileName = FileName id |> FileName.setExtension "json"
+        dataEntriesDirPath/fileName
+
+    member this.ReadAll() =
+        if not (dataEntriesDirPath |> Util.IO.Directory.exists) then Seq.empty
+        else
+            Util.IO.Directory.listFiles dataEntriesDirPath
+            |> Seq.map Util.IO.File.readAllText
+
+    member this.Write (id: string) (jsonString: string) =
+        let entryFilePath = getEntryFilePath id
+        if entryFilePath |> Util.IO.File.exists then Util.IO.File.delete entryFilePath
+        Util.IO.File.writeText entryFilePath jsonString
+
+    member this.Delete id = getEntryFilePath id |> Util.IO.File.delete
+
+    member this.HasFieldValue fieldName value =
+        if not (dataEntriesDirPath |> Util.IO.Directory.exists) then false
+        else 
+            Util.IO.Directory.listFiles dataEntriesDirPath
+            |> Seq.exists (jsonFieldValueComparer fieldName value)
+
+    member this.HasId id = getEntryFilePath id |> Util.IO.File.exists
+
+    member this.FindByFieldValue fieldName (value: 'b) =
+        Util.IO.Directory.listFiles dataEntriesDirPath
+        |> Seq.find (jsonFieldValueComparer fieldName value)
+        |> Util.Json.deserializeFile<'Record>
+
+    member this.FindById (id: string) = getEntryFilePath id |> Util.IO.File.readAllText
+
+    member this.UpdateFieldValue id (fieldName: string) value =
+        let entryFilePath = getEntryFilePath id
+        let json = Util.Json.read entryFilePath
+        json[fieldName] <- value
+        Util.IO.File.writeText entryFilePath (json.ToString())
