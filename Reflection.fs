80d7f720dd1d458101fe44d9c528d5bf0645e692
dmrysev <dmrysev@protonmail.com>
2022-02-10 22:44:55 +0200

    Fixed loaging reflection plugins

--- Reflection.fs
+++ Reflection.fs
@@ -4,16 +4,23 @@ open Util.IO.Path
 open System
 open System.Reflection
 
+let loadAssembly assemblyFilePath =
+    try 
+        let types = (Assembly.LoadFile assemblyFilePath).GetTypes()
+        Some types
+    with | :? System.BadImageFormatException -> None
+
 let loadPlugins<'a>(pluginsDirPath: DirectoryPath) =
-    Util.IO.Directory.listDirectories pluginsDirPath
-    |> Seq.map (fun pluginDirPath -> 
-        Util.IO.Directory.listFiles pluginDirPath
-        |> Seq.map FilePath.value
-        |> Seq.filter (Util.Regex.isMatch @".+\.dll") )
-    |> Seq.concat
-    |> Seq.map (fun path -> (Assembly.LoadFile path).GetTypes() )
-    |> Seq.concat
-    |> Seq.filter(fun assemblyType -> typeof<'a>.IsAssignableFrom(assemblyType) )
-    |> Seq.map(fun assemblyType -> Activator.CreateInstance(assemblyType) )
-    |> Seq.filter(fun x -> not (x |> isNull) )
-    |> Seq.cast<'a>
+    if pluginsDirPath |> Util.IO.Directory.exists then
+        Util.IO.Directory.listDirectories pluginsDirPath
+        |> Seq.collect (fun pluginDirPath -> 
+            Util.IO.Directory.listFiles pluginDirPath
+            |> Seq.map FilePath.value
+            |> Seq.filter (Util.Regex.isMatch @".+\.dll") )
+        |> Seq.choose loadAssembly
+        |> Seq.concat
+        |> Seq.filter(fun assemblyType -> typeof<'a>.IsAssignableFrom(assemblyType) )
+        |> Seq.map(fun assemblyType -> Activator.CreateInstance(assemblyType) )
+        |> Seq.filter(fun x -> not (x |> isNull) )
+        |> Seq.cast<'a>
+    else Seq.empty
