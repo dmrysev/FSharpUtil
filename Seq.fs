2e6e6fc8a5a3415754f24ccdb271435de050e8b4
dmrysev <dmrysev@protonmail.com>
2022-06-25 23:07:00 +0300

    In Seq added replace

--- Seq.fs
+++ Seq.fs
@@ -5,3 +5,8 @@ let isSubset superSet set = set |> Seq.except superSet |> Seq.isEmpty
 let hasOverlap seqA seqB = 
     let resultSeqA = seqA |> Seq.except seqB
     (resultSeqA |> Seq.length) < (seqA |> Seq.length)
+
+let replace oldItem newItem seq = 
+    seq
+    |> Seq.except [oldItem]
+    |> Seq.append [newItem]
