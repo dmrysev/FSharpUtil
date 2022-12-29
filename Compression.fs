3c27ddb09aac2317172d1c519964ad69464d019a
dmrysev <dmrysev@protonmail.com>
2022-12-29 14:19:56 +0200

    Added Util.Compression.Zip.extract

--- Compression.fs
+++ Compression.fs
@@ -18,3 +18,6 @@ module Zip =
     let compressDirectory (inputDirPath: DirectoryPath) (outputArchiveFilePath: FilePath) (compressionLevel: CompressionLevel) =
         let compressionLevelInt = compressionLevel |> compressionLevelToInt
         Util.Process.execute $"7z a -tzip -mx={compressionLevelInt} {outputArchiveFilePath.Value} {inputDirPath.Value}" |> ignore
+
+    let extract (inputArchive: FilePath) (outputDirPath: DirectoryPath) =
+        Util.Process.execute $"7z x '{inputArchive.Value}' -o'{outputDirPath.Value}'" |> ignore
