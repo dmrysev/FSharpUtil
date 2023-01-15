53c65725ceb535ac0e1149a85432965eea088437
dmrysev <dmrysev@protonmail.com>
2023-01-15 12:56:34 +0200

    Improvements to Reflection

--- Reflection.fs
+++ Reflection.fs
@@ -4,20 +4,12 @@ open Util.IO.Path
 open System
 open System.Reflection
 
-let loadAssembly assemblyFilePath =
-    try 
-        let types = (Assembly.LoadFile assemblyFilePath).GetTypes()
-        Some types
-    with | :? System.BadImageFormatException -> None
-
 let loadPlugins<'a>(pluginsDirPath: DirectoryPath) =
     if pluginsDirPath |> Util.IO.Directory.exists then
-        Util.IO.Directory.listDirectories pluginsDirPath
-        |> Seq.collect (fun pluginDirPath -> 
-            Util.IO.Directory.listFiles pluginDirPath
-            |> Seq.map FilePath.value
-            |> Seq.filter (Util.Regex.isMatch @".+\.dll") )
-        |> Seq.choose loadAssembly
+        Util.IO.Directory.listFilesRecursivePattern pluginsDirPath "*.dll"
+        |> Seq.choose (fun assemblyFilePath ->
+            try Some ((Assembly.LoadFile assemblyFilePath.Value).GetTypes())
+            with error -> None)
         |> Seq.concat
         |> Seq.filter(fun assemblyType -> typeof<'a>.IsAssignableFrom(assemblyType) )
         |> Seq.map(fun assemblyType -> Activator.CreateInstance(assemblyType) )
