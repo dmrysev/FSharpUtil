b80cbf76079cc0fa752b59507a2563794adcf723
dmrysev <dmrysev@protonmail.com>
2023-02-26 22:07:14 +0200

    Added String.head

--- String.fs
+++ String.fs
@@ -14,6 +14,7 @@ let contains (subString: string) (str: string) = str.Contains(subString)
 let startsWith (subString: string) (str: string) = str.StartsWith(subString)
 let endsWith (subString: string) (str: string) = str.EndsWith(subString)
 let defaultIfEmpty (subString: string) (str: string) = if str = "" then subString else str
+let head (endIndex: int) (str: string) = str.Substring (0, endIndex)
 let tail (startIndex: int) (str: string) = str.Substring (startIndex, str.Length - startIndex)
 let slice (startIndex: int) (endIndex: int) (str: string) = str.Substring (startIndex, endIndex - startIndex + 1)
 
