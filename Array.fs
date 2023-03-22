b118909451fed79ba47b472d9e53e387cd7a043e
dmrysev <dmrysev@protonmail.com>
2023-03-22 20:11:39 +0200

    Added Array module

---
+++ Array.fs
@@ -0,0 +1,13 @@
+module Util.Array
+
+let rng = new System.Random()
+let shuffle (arr : 'a array) =
+    let array = Array.copy arr
+    let n = array.Length
+    for x in 1..n do
+        let i = n-x
+        let j = rng.Next(i+1)
+        let tmp = array.[i]
+        array.[i] <- array.[j]
+        array.[j] <- tmp
+    array
