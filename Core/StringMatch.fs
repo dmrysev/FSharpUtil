284c2ca75f4c391bdd575fd131e9d016aad743c2
dmrysev <dmrysev@protonmail.com>
2025-03-08 12:01:52 +0200

    Added image string match

--- Core/StringMatch.fs
+++ Core/StringMatch.fs
@@ -1,6 +1,6 @@
 module Util.StringMatch
 
-let isImageFile text = text |> Util.Regex.isMatchIgnoreCase @"^.+\.(png|jpg|jpeg)$"
+let isImageFile text = text |> Util.Regex.isMatchIgnoreCase @"^.+\.(png|jpg|jpeg|webp)$"
 let isVideoFile text = text |> Util.Regex.isMatchIgnoreCase @"^.+\.(webm|flv|vob|ogg|ogv|drc|gifv|mng|avi|mov|qt|wmv|yuv|rm|rmvb|asf|amv|mp4|m4v|mp*|m?v|svi|3gp|flv|f4v|gif|mpeg|mpg|mkv|ts)$"
 let isComicBookFile text = text |> Util.Regex.isMatchIgnoreCase @"^.+\.(cbz|cbr|cb7)$"
 let notEmpty text = text <> ""
